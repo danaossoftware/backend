044ba2ef1ceeda3aa1cd33ad35df10db0dcfbdb4
Dana Prakoso <danaossoftware@gmail.com>
2019-01-13 13:25:52 +0700

    commit

--- scripts/home.js
+++ scripts/home.js
@@ -2949,7 +2949,6 @@ function saveEdittedCourse() {
         $("#edit-course-error").css("display", "block");
         return;
     }
-    alert(lecturer);
     $.ajax({
         type: 'GET',
         url: PHP_PATH+'edit-course.php',
@@ -2957,7 +2956,6 @@ function saveEdittedCourse() {
         dataType: 'text',
         cache: false,
         success: function(a) {
-            alert(a);
             $("#edit-course-container").css("display", "none");
             getCourses();
         },
