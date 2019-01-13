e68ec1dbf4e6259325026134e8b77ca51daca53c
Dana Prakoso <danaossoftware@gmail.com>
2019-01-13 11:37:12 +0700

    commit

--- scripts/edit-course.php
+++ scripts/edit-course.php
@@ -3,7 +3,7 @@ include 'db.php';
 $id = $_GET["id"];
 $name = $_GET["name"];
 $lecturer = $_GET["lecturer"];
-if ($c->query("UPDATE courses SET name='" . $name . "' AND lecturer='" . $lecturer . "' WHERE id='" . $id . "'")) {
+if ($c->query("UPDATE courses SET name='" . $name . "', lecturer='" . $lecturer . "' WHERE id='" . $id . "'")) {
     echo 0;
 } else {
     echo -1;
