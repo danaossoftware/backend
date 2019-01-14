ba1f92e0b89ce651e5736e8daaf252fb97552f84
Dana Prakoso <danaossoftware@gmail.com>
2019-01-15 00:00:00 +0700

    commit

--- scripts/home.js
+++ scripts/home.js
@@ -1441,7 +1441,7 @@ function editQuestion(btnEditQuestion) {
         $("#edit-question-audio-source").attr("src", "");
         $("#edit-question-audio")[0].load();
     }
-    alert(question.reason);
+    alert(coursesJSON.toString());
     $("#edit-question-reason").val(question.reason);
     var splittedAnswer = answers.split("@");
     if (type == 'pilihan') {
