9bc9d8a67cd96f3c777031d5795e9939b67d2a7f
Dana Prakoso <danaossoftware@gmail.com>
2019-01-16 16:25:21 +0700

    commit

--- scripts/edit-bab.php
+++ scripts/edit-bab.php
@@ -1,7 +1,6 @@
 <?php
 include 'db.php';
-/*$id = $_GET["id"];
+$id = $_GET["id"];
 $name = $_GET["name"];
 $accessCode = $_GET["access_code"];
-$c->query("UDDATE bab SET name='" . $name . "', access_code='" . $accessCode . "' WHERE id='" . $id . "'");*/
-$c->query("UPDATE bab SET name='HTML2', access_code='123abc456' WHERE id='5c3c80ca481c1'");
+$c->query("UPDATE bab SET name='" . $name . "', access_code='" . $accessCode . "' WHERE id='" . $id . "'");
