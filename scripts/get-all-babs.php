2ca1738b398db850b0cfaa1a4d4b3713054ece2b
Dana Prakoso <danaossoftware@gmail.com>
2019-01-16 16:26:30 +0700

    commit

--- scripts/get-all-babs.php
+++ scripts/get-all-babs.php
@@ -8,7 +8,7 @@ if (!$results) {
 if ($results->num_rows > 0) {
     $response = "[";
     while ($row = $results->fetch_assoc()) {
-        $response .= ("{\"id\": \"" . $row["id"] . "\", \"name\": \"" . $row["name"] . "\", \"course_id\": \"" . $row["course_id"] . "\", \"img_url\": \"" . $row["img_url"] . "\"}, ");
+        $response .= ("{\"id\": \"" . $row["id"] . "\", \"name\": \"" . $row["name"] . "\", \"course_id\": \"" . $row["course_id"] . "\", \"access_code\": \"" . $row["access_code"] . "\", \"img_url\": \"" . $row["img_url"] . "\"}, ");
     }
     $response = substr($response, 0, strlen($response)-2);
     $response .= "]";
